e024cb58f4af7d5049b802c1f2e0ce04e619e10a
kost01 <yookunsa@gmail.com>
2018-05-26 15:19:05 +0900

    commit

--- src/com/ex/Hello.java
+++ src/com/ex/Hello.java
@@ -3,7 +3,7 @@ package com.ex;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello World!!");
 		System.out.println("Hello World");
 	}
 }
